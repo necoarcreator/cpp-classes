7ff917f7c4a9cc3495773b37de7e74da5340d31b
necoarcreator <145058771+necoarcreator@users.noreply.github.com>
2023-11-17 15:21:49 +0300

    Add files via upload

---
+++ LIFO_FIFO/lifofifo_template/lifofifo_template.cpp
@@ -0,0 +1,26 @@
+﻿#include "Stack.h"
+#include "Queue.h"
+
+
+int main()
+{
+    vector <int> A{ 1,2,3,4,5};
+    Stack inpt {-6, -10, -15, 15, 2};
+    //Stack empt;
+    inpt.push(6);
+    int B = inpt.pop();
+
+    cout << B;
+    cout << endl;
+    inpt.print();
+    int C = inpt.pop();
+    /*Queue inptt(vector<int>{1, 2, 3});
+    inptt.push(6);
+    int B = inptt.pop();
+
+    cout << B;
+    cout << endl;
+    inptt.print();
+    return 0;
+    */
+}
