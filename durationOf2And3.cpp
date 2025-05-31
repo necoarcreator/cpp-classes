67419e3c9cad7df7f8a8c569385f5604b1094281
necoarcreator <145058771+necoarcreator@users.noreply.github.com>
2025-05-31 21:12:28 +0300

    Add files via upload

---
+++ durationOf2And3.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <ctime>
+
+bool evenOdd(int n)
+{
+    if (n % 2)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool prove(int n)
+{
+    if (n % 3)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+bool evenOddBit(int n)
+{
+    int spr = 0b0001;
+
+    if (spr & n)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+}
+
+//#pragma optimize("", off)
+
+int main()
+{
+    int i, j, n;
+    long long N = 10e7;
+    std::clock_t start;
+    double duration_1, duration_2;
+
+    srand(time(NULL));
+
+    n = rand();
+    start = std::clock();
+    for (i = 0; i < N; i++)
+    {
+        n = rand();
+        prove(n);
+    }
+    duration_1 = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+
+    n = rand();
+    start = std::clock();
+    for (i = 0; i < N; i++)
+    {
+        n = rand();
+        evenOdd(n);
+    }
+    duration_2 = (std::clock() - start) / (double)CLOCKS_PER_SEC;
+
+    std::cout << " The time for 2 is  " << duration_1 << std::endl;
+    std::cout << " The time for 3 is  " << duration_2 << std::endl;
+
+    std::cout << " The percent is  " << duration_2/duration_1 << "%" << std::endl;
+}
+//#pragma optimize("", on)
---
+++ memoryAlloc.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+class A
+{
+    private:
+
+    int a, b, c;
+
+    public:
+
+        
+
+        A(int a1, int b1, int c1) : a(a1), b(b1), c(c1) {}
+
+};
+
+int main()
+{
+    A a1(1, 2, 3);
+    A* a1_ptr = &a1;
+    A** a1_ptr_ptr = &a1_ptr;
+    int* k = (int*)a1_ptr;
+    cout << "*a1_ptr = " << *a1_ptr << endl;
+    cout << "*k = " << *k << endl;
+    cout << "*(k + 1)= " << *(k + 1) << endl;
+    cout << "*(k + 2)= " << *(k + 2) << endl;
+
+    cout << "mem k = " << sizeof(k) << endl;
+    cout << "mem (k + 1)= " << sizeof((k + 1)) << endl;
+    cout << "mem (k + 2)= " << sizeof((k + 2)) << endl;
+
+    cout << "mem a1_ptr = " << sizeof(a1_ptr) << endl;
+    cout << "mem a1_ptr_ptr = " << sizeof(a1_ptr_ptr) << endl;
+}
